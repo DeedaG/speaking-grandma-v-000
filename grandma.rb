41a03923255f01c515c5870402cd80ad2471c6e1
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-16 03:38:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrases)
 
   if phrase == "I LOVE YOU GRANDMA!"
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    puts "I LOVE YOU TOO PUMPKIN!"
 
     else
       puts "HUH?! SPEAK UP, SONNY!"
