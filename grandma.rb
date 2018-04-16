315a3c9d51bd18e627242a896da05295fdfed563
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-16 03:03:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
+  phrase = gets.string
   if phrase.downcase == true
     puts "HUH?! SPEAK UP SONNY!"
   elsif phrase.upcase == true
