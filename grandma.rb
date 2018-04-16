fb8afd3dc18d0cdd1815ee0f029c77bb911cc185
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-16 02:53:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == (/[a-z]/)
     puts "HUH?! SPEAK UP SONNY!"
-  elsif phrase == [A-Z]
+  elsif phrase == (/[A-Z]/)
     puts "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
