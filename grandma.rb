7c7da9585ada0b82212a683c7b5f0c7acce514e2
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-16 03:26:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase || phrase.capitalize
-    puts "HUH?! SPEAK UP SONNY!"
-  elsif "phrase".upcase == true
+  
+  if phrase == phrase.upcase 
     puts "NO, NOT SINCE 1938!"
   elsif "phrase" == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
